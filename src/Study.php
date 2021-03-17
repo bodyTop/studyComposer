e53cf7871a99cd02707472689f1c878485bd8dd4
18822065775 <13835610017@163.com>
2021-03-17 11:24:15 +0800

    init

---
+++ src/Study.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Guanguans;
+
+
+class Study
+{
+
+    public function test(){
+        return 'ok';
+    }
+
+
+    public function newSum(int $a, int $b){
+
+        return $a+$b;
+    }
+}
